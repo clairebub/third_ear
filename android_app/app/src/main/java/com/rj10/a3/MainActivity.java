bdc242f3025615763967034e4f9af127faed93f5
Claire Tang <claire.smurfs@gmail.com>
2017-11-28 16:30:07 -0800

    cleaned up the code a bit for dual mode support.

--- android_app/app/src/main/java/com/rj10/a3/MainActivity.java
+++ android_app/app/src/main/java/com/rj10/a3/MainActivity.java
@@ -30,20 +30,145 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity
         implements ActivityCompat.OnRequestPermissionsResultCallback {
 
-    private final int REQ_CODE_SPEECH_RECOG_LOCAL = 100;
     private final int PERMISSION_REQUEST_READ_PHONE_STATE = 1;
     private final int PERMISSION_REQUEST_RECORD_AUDIO = 2;
+
+    private final int REQ_CODE_SPEECH_RECOG_LOCAL = 100;
 
     Button mOnButton;
     TextView mStatus;
     RadioGroup mRadioGroup;
 
-    SpeechResultReceiver speechResultReceiver = null;
+    CloudSpeechResultReceiver mCloudSpeechResultReceiver = null;
     private VoiceRecorder mVoiceRecorder;
     private SpeechService mSpeechService;
     private SpeechApiService mSpeechApiService;
 
     int mRequestCount = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mStatus = (TextView) findViewById(R.id.status);
+        mStatus.setHint("Version: 0.1c");
+        mOnButton = (Button) findViewById(R.id.buttonOn);
+        mOnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleOnClick();
+            }
+        });
+
+        mRadioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        mRadioGroup.check(R.id.radioButton_local);
+        mRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+                RadioButton rb = (RadioButton) radioGroup.findViewById(checkedId);
+                if (null != rb && checkedId > -1) {
+                    Toast.makeText(MainActivity.this, rb.getText(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        // Bind to SpeechApiService
+        Intent intent = new Intent(this, SpeechApiService.class);
+        bindService(intent, mSpeechApiConnection, Context.BIND_AUTO_CREATE);
+
+        mCloudSpeechResultReceiver = new CloudSpeechResultReceiver(new Handler());
+    }
+
+    private void handleOnClick() {
+        mRequestCount++;
+        String msg = String.format("Starting request #%d", mRequestCount);
+        msg = "foo: " + mSpeechApiService.foo();
+        mStatus.setText(msg);
+        // startSpeechRec();
+
+        int radioButtonID = mRadioGroup.getCheckedRadioButtonId();
+        switch (radioButtonID) {
+            case R.id.radioButton_local:
+                startSpeechRecLocal();
+                break;
+            case R.id.radioButton_cloud:
+                startSpeechRecCloud();
+                break;
+            default:
+                deebug("unexpected radioButtonId: " + radioButtonID);
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_RECOG_LOCAL: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    deebug(result.get(0));
+                }
+                break;
+            }
+            default:
+                deebug("unexpected request code: " + requestCode);
+                break;
+        }
+    }
+
+    private void startSpeechRecLocal() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(
+                RecognizerIntent.EXTRA_PROMPT,
+                "Starting speech recognition...");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_RECOG_LOCAL);
+        } catch (ActivityNotFoundException a) {
+            deebug(a.toString());
+        }
+    }
+
+    private void startSpeechRecCloud() {
+        checkPermissions(Manifest.permission.RECORD_AUDIO, PERMISSION_REQUEST_RECORD_AUDIO);
+
+        if (mVoiceRecorder != null) {
+            mVoiceRecorder.stop();
+        }
+        mVoiceRecorder = new VoiceRecorder(mVoiceCallback);
+        mVoiceRecorder.start();
+
+        Intent intent = new Intent(this, SpeechService.class);
+        intent.putExtra("receiver", mCloudSpeechResultReceiver);
+        intent.putExtra("sender", "stem main");
+        startService(intent);
+    }
+
+
+    private void stopSpeechRecCloud() {
+        if (mVoiceRecorder != null) {
+            mVoiceRecorder.stop();
+            mVoiceRecorder = null;
+        }
+    }
+
+    private void startSpeechRecCloud2() {
+        checkPermissions(Manifest.permission.READ_PHONE_STATE, PERMISSION_REQUEST_READ_PHONE_STATE);
+        checkPermissions(Manifest.permission.RECORD_AUDIO, PERMISSION_REQUEST_RECORD_AUDIO);
+
+        Intent intent = new Intent(this, SpeechService.class);
+        intent.putExtra("receiver", mCloudSpeechResultReceiver);
+        intent.putExtra("sender", "stem main");
+        startService(intent);
+
+    }
 
     //
     // Callback from voice recorder to handle start/ongoing/end of voice input.
@@ -80,131 +205,8 @@ public class MainActivity extends AppCompatActivity
         }
     };
 
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        speechResultReceiver = new SpeechResultReceiver(new Handler());
-
-        mStatus = (TextView) findViewById(R.id.status);
-        mStatus.setHint("Version: 0.1c");
-        mOnButton = (Button) findViewById(R.id.buttonOn);
-        mOnButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                handleOnClick();
-            }
-        });
-
-        mRadioGroup = (RadioGroup) findViewById(R.id.radioGroup);
-        mRadioGroup.check(R.id.radioButton_local);
-        mRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
-                RadioButton rb = (RadioButton) radioGroup.findViewById(checkedId);
-                if (null != rb && checkedId > -1) {
-                    Toast.makeText(MainActivity.this, rb.getText(), Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        // Bind to SpeechApiService
-        Intent intent = new Intent(this, SpeechApiService.class);
-        bindService(intent, mSpeechApiConnection, Context.BIND_AUTO_CREATE);
-    }
-
-    private void startSpeechRecLocal() {
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(
-                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        intent.putExtra(
-                RecognizerIntent.EXTRA_PROMPT,
-                "Starting speech recognition...");
-        try {
-            startActivityForResult(intent, REQ_CODE_SPEECH_RECOG_LOCAL);
-        } catch (ActivityNotFoundException a) {
-            deebug(a.toString());
-        }
-    }
-
-    private void startSpeechRecCloud() {
-        checkPermissions(Manifest.permission.RECORD_AUDIO, PERMISSION_REQUEST_RECORD_AUDIO);
-
-        if (mVoiceRecorder != null) {
-            mVoiceRecorder.stop();
-        }
-        mVoiceRecorder = new VoiceRecorder(mVoiceCallback);
-        mVoiceRecorder.start();
-
-        Intent intent = new Intent(this, SpeechService.class);
-        intent.putExtra("receiver", speechResultReceiver);
-        intent.putExtra("sender", "stem main");
-        startService(intent);
-    }
-
-    private void handleOnClick() {
-        mRequestCount++;
-        String msg = String.format("Starting request #%d", mRequestCount);
-        msg = "foo: " + mSpeechApiService.foo();
-        mStatus.setText(msg);
-        // startSpeechRec();
-
-        int radioButtonID = mRadioGroup.getCheckedRadioButtonId();
-        if (radioButtonID == R.id.radioButton_local) {
-            startSpeechRecLocal();
-        } else {
-            deebug("unexpected radioButtonId: " + radioButtonID);
-        }
-
-    }
-    private void stopSpeechRec() {
-        if (mVoiceRecorder != null) {
-            mVoiceRecorder.stop();
-            mVoiceRecorder = null;
-        }
-    }
-
-    /*
-    void startSpeedRec() {
-        checkPermissions(Manifest.permission.READ_PHONE_STATE, PERMISSION_REQUEST_READ_PHONE_STATE);
-        checkPermissions(Manifest.permission.RECORD_AUDIO, PERMISSION_REQUEST_RECORD_AUDIO);
-
-        Intent intent = new Intent(this, SpeechService.class);
-        intent.putExtra("receiver", speechResultReceiver);
-        intent.putExtra("sender", "stem main");
-        startService(intent);
-
-    }
-*/
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        switch (requestCode) {
-            case REQ_CODE_SPEECH_RECOG_LOCAL: {
-                if (resultCode == RESULT_OK && null != data) {
-                    ArrayList<String> result = data
-                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                   deebug(result.get(0));
-                }
-                break;
-            }
-            default:
-                deebug("unexpected request code: " + requestCode);
-                break;
-        }
-    }
-
-    public class SpeechResultReceiver extends ResultReceiver {
-        public SpeechResultReceiver(Handler handler) {
+    public class CloudSpeechResultReceiver extends ResultReceiver {
+        public CloudSpeechResultReceiver(Handler handler) {
             super(handler);
         }
 
@@ -220,6 +222,22 @@ public class MainActivity extends AppCompatActivity
             });
         }
     }
+
+    /** Defines callbacks for service binding, passed to bindService() */
+    private final ServiceConnection mSpeechApiConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            SpeechApiService.SpeechApiBinder binder = (SpeechApiService.SpeechApiBinder) service;
+            mSpeechApiService = binder.getService();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            mSpeechApiService = null;
+        }
+    };
 
     private void checkPermissions(String permission, int requestCode) {
         if (ContextCompat.checkSelfPermission(this, permission)
@@ -253,25 +271,7 @@ public class MainActivity extends AppCompatActivity
             super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
-
-
-
-    /** Defines callbacks for service binding, passed to bindService() */
-    private final ServiceConnection mSpeechApiConnection = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName className,
-                                       IBinder service) {
-            // We've bound to LocalService, cast the IBinder and get LocalService instance
-            SpeechApiService.SpeechApiBinder binder = (SpeechApiService.SpeechApiBinder) service;
-            mSpeechApiService = binder.getService();
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName arg0) {
-            mSpeechApiService = null;
-        }
-    };
-
+    
     void showStatus(final String msg) {
         runOnUiThread(new Runnable() {
             @Override
