0ef5de9f9c1b0ae18bfd484b691574ed9aad5be3
Claire Tang <claire.smurfs@gmail.com>
2018-01-27 18:05:58 -0800

    getting to know more about the tarsos package.

--- mfcc_test/src/main/java/MFCCTest.java
+++ mfcc_test/src/main/java/MFCCTest.java
@@ -14,57 +14,55 @@ public class MFCCTest {
 //	private static int counter = 0;
 
 	//@Test
-	public List<float[]> Test() {
-		String path = "data/test.wav";
-		int sampleRate = 16000;
-		int bufferSize = 1024;
-		int bufferOverlap = 128;
-		//final float[] floatBuffer = TestUtilities.audioBufferSine();
-		// PipedAudioStream file = new PipedAudioStream("test.wav");
-		// TarsosDSPAudioInputStream stream = file.getMonoStream(16000,0);
-		// System.out.println(stream);
-		InputStream inStream = null;
-		File f = new File(path);
-		try{
-         inStream = new FileInputStream(f);
-      	}
-      	catch (FileNotFoundException ex) {
-        }
+	public List<float[]> Test() throws FileNotFoundException {
+		final String path = "data/test.wav";
+		final int sampleRate = 16000;
+		final int bufferSize = 1024;
+		final int bufferOverlap = 128;
+		final int samplesPerFrame = -1;
+		final int amountOfCepstrumCoef = 20;
+		final int amountOfMelFilters = 50;
+		final int lowerFilterFreq = 25;
+		final int upperFilterFreq = 200000;
 
-		//AudioDispatcher dispatcher = new AudioDispatcher(stream, bufferSize, bufferOverlap);
-		final List<float[]>mfccList = new ArrayList<>(200);
-        AudioDispatcher dispatcher = new AudioDispatcher(new UniversalAudioInputStream(inStream, new TarsosDSPAudioFormat(sampleRate, 16, 1, true, true)), bufferSize, bufferOverlap);
-		// //final AudioDispatcher dispatcher = AudioDispatcherFactory.fromPipe("test.wav", sampleRate, bufferSize, bufferOverlap);
-		final MFCC mfcc = new MFCC(bufferSize, sampleRate, 20, 50, 25, 200000);
+		InputStream inStream = new FileInputStream(path);
+			TarsosDSPAudioFormat audioFormat = new TarsosDSPAudioFormat(
+				sampleRate, 16, 1, true, true);
+		UniversalAudioInputStream audioInputStream = new UniversalAudioInputStream(
+				inStream, audioFormat);
+		AudioDispatcher dispatcher = new AudioDispatcher(
+				audioInputStream, bufferSize, bufferOverlap);
+		System.out.println("dispatcher.format(): " + dispatcher.getFormat());
+		final List<float[]>mfccList = new ArrayList<>();
+		final MFCC mfcc = new MFCC(
+		    bufferSize,
+				sampleRate,
+				amountOfCepstrumCoef,
+				amountOfMelFilters,
+				lowerFilterFreq,
+				upperFilterFreq);
 		dispatcher.addAudioProcessor(mfcc);
-
 		dispatcher.addAudioProcessor(new AudioProcessor() {
 			@Override
 			public boolean process(AudioEvent audioEvent) {
-				System.out.println(Arrays.toString(mfcc.getMFCC()));
 				mfccList.add(mfcc.getMFCC());
 				return true;
 			}
 			@Override
-			public void processingFinished() {
-				System.out.println("enter processingFinished");
-			}
+			public void processingFinished() {}
 		});
+
 		dispatcher.run();
-		System.out.println(dispatcher.getFormat());
-		System.out.println("mfcc size: " + mfccList.size());
 		return mfccList;
 	}
-	public void floatArrayPrint(float [] farray){
 
-		for(int i = 0; i < farray.length; i++){
-			System.out.print(farray[i]);
-		}
-	}
-	public static void main(String[] args){
+	public static void main(String[] args) throws FileNotFoundException {
 		MFCCTest a = new MFCCTest();
 		List<float[]> mfcc = a.Test();
-
+		System.out.println("mfcc size: " + mfcc.size());
+		System.out.println("mfcc values: ");
+		for (float[] x: mfcc) {
+			System.out.println(Arrays.toString(x));
+		}
 	}
-
 }
