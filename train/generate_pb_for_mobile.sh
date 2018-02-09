36bc943147387d56fef81eb56d901b5215bb58d4
Claire Tang <claire.smurfs@gmail.com>
2018-02-08 21:22:37 -0800

    Now check model and loading it in mobile.

--- train/generate_pb_for_mobile.sh
+++ train/generate_pb_for_mobile.sh
@@ -2,11 +2,19 @@
 
 TOOLS_DIR=/Users/rjtang/_hack/tensorflow/tensorflow/python/tools
 
+model_dir=/tmp/stem/model-20180208-211356
+echo "model_dir=$model_dir"
+
 cd ${TOOLS_DIR}
 pwd
 python freeze_graph.py \
-  --input_graph=/tmp/tfdriod/tfdroid.pbtxt \
+  --input_graph=$model_dir/model.pbtxt \
   --input_binary=false \
-  --input_checkpoint=/tmp/tfdriod/tfdroid.ckpt \
+  --input_checkpoint=/tmp/stem/model-20180208-211356/ckpt \
   --output_graph=/tmp/frozen_graph.pb \
   --output_node_names=W
+
+#python freeze_graph.py \
+#  --input_saved_model_dir=$model_dir \
+#  --output_graph=sound_model.pb \
+#  --output_node_names=W
