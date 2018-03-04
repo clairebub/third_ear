ef7dc0e41223a5fafe564ada6d7da0837310a4f9
Tang Family <tangfamily@mbp-15.local>
2018-03-04 02:25:55 -0800

    traing for 2k steps now

--- us2/dnn.py
+++ us2/dnn.py
@@ -8,7 +8,7 @@ import urban_sound_data
 parser = argparse.ArgumentParser()
 parser.add_argument('--batch_size', default=100, type=int,
                     help='batch size')
-parser.add_argument('--train_steps', default=20, type=int,
+parser.add_argument('--train_steps', default=1000, type=int,
                     help='number of training steps')
 
 def main(argv):
